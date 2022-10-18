0a878ef71505ebc33ba702b1d69efe0e29ca4157
shubham-2094 <shubham2094@yahoo.com>
2022-10-18 09:27:17 +0530

    props method

--- src/index.js
+++ src/index.js
@@ -4,42 +4,49 @@ import ReactDOM from "react-dom";
 //css
 import "./index.css";
 
+const firstBook = {
+  author: "Joseph Murphy",
+  title: "The Power of Your Subconscious Mind",
+  price: "₹105",
+  img: "https://m.media-amazon.com/images/I/71UwSHSZRnS._AC_UY327_FMwebp_QL65_.jpg",
+};
+
+const secondBook = {
+  author: "Morgan Housel",
+  title: "The Psychology of Money",
+  price: "₹235",
+  img: "https://m.media-amazon.com/images/I/71g2ednj0JL._AC_UY327_FMwebp_QL65_.jpg",
+};
+
 function BookList() {
   return (
     <div className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        price={firstBook.price}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        price={secondBook.price}
+        img={secondBook.img}
+      />
     </div>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
-    <article className="book">
-      <Image />
-      <Titlte />
-      <Author />
-      <Price />
+    <article className="book  ">
+      <img src={props.img} alt="" />
+      <h4>{props.title}</h4>
+      <h5>{props.author} </h5>
+      <h6> {props.price}</h6>
     </article>
   );
 };
-
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/71UwSHSZRnS._AC_UY327_FMwebp_QL65_.jpg"
-      alt=""
-    />
-  );
-};
-
-const Titlte = () => <h4>The Power of Your Subconscious Mind</h4>;
-const Author = () => <h5> Joseph Murphy</h5>;
-const Price = () => <h6>₹105 </h6>;
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
